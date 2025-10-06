d716caebffca5589288a5fca97b0d46663710251
Jakub <56446320+Niewiaro@users.noreply.github.com>
2025-10-06 13:20:37 +0200

    [feat] info directory
    Changed the '/info' route to send an information page response and removed middleware logging. Replaced manual server creation with app.listen for simplicity.

--- app.js
+++ app.js
@@ -3,15 +3,14 @@ const express = require('express');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-    console.log('First middleware.');
-    next();
+app.use('/info', (req, res, next) => {
+    res.send('<h1>We are now at the \"Information\" page.</h1>');
 });
 
 app.use('/', (req, res, next) => {
-    console.log('Second middleware.');
     res.send('<h1>Back-end programming, lab 1, Hello from NODE & Express.js!</h1>');
 });
 
-const server = http.createServer(app);
-server.listen(33333);
+// const server = http.createServer(app);
+// server.listen(33333);
+app.listen(33333);
