de051b688a030b5c014cf48f3208c9eb99fe8f94
Jakub <56446320+Niewiaro@users.noreply.github.com>
2025-10-06 13:33:58 +0200

    [feat] product POST
    Introduced a new '/add-product' route with a form to submit product names and a '/product' POST route to handle form submissions. Enabled URL-encoded body parsing for form data.

--- app.js
+++ app.js
@@ -3,9 +3,24 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.urlencoded({extended: false}));
+
 app.use('/info', (req, res, next) => {
     res.send('<h1>We are now at the \"Information\" page.</h1>');
 });
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<h1>Insert product name</h1>' + 
+        '<form action="/product" method="POST"' + 
+        '<input type="text" name="title" />' + 
+        '<button type="submit">Send product name</button>' + 
+        '</form>');
+});
+
+app.post('/product', (req, res, next)=>{
+    console.log('Received data:', req.body);
+    res.redirect('/');
+})
 
 app.use('/', (req, res, next) => {
     res.send('<h1>Back-end programming, lab 1, Hello from NODE & Express.js!</h1>');
