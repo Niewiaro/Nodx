e98cdb3c4fd7b712411f2f1b4cc2d6676fbde600
Jakub <56446320+Niewiaro@users.noreply.github.com>
2025-10-19 22:03:35 +0200

    [chore] remove status

--- routes/shop.js
+++ routes/shop.js
@@ -12,7 +12,7 @@ router.get('/about', (req, res, next) => {
 })
 
 router.get('/', (req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
+    res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
 })
 
 
