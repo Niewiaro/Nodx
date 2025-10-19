674445841860d83dac7fb08f6ff3d2edced9229f
Jakub <56446320+Niewiaro@users.noreply.github.com>
2025-10-19 22:11:02 +0200

    [feat] add info routes and update links to about page

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const path = require('path');
 const express = require('express');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const infoRoutes = require('./routes/info');
 
 const app = express();
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use('/admin', adminRoutes);
+app.use('/info', infoRoutes);
 
 app.use((req, res, next)=>{
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
--- routes/info.js
+++ routes/info.js
@@ -3,8 +3,8 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
+router.get('/about', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'about.html'));
 })
 
 module.exports = router;
