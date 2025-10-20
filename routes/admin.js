35a4b93bef95631bfc12d777e88a6dcc91738732
Jakub <56446320+Niewiaro@users.noreply.github.com>
2025-10-20 13:09:02 +0200

    [init]

--- routes/admin.js
+++ routes/admin.js
@@ -2,15 +2,20 @@ const path = require('path');
 const express = require('express');
 const router = express.Router();
 
+const products = [];
 
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
+  res.sendFile(path.join(path.dirname(require.main.filename), 
+  'views', 'add-product.html'));
 });
 
+// /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-    console.log('Received data:', req.body);
-    res.redirect('/');
-})
+  // console.log(req.body);
+  products.push({ title: req.body.title });
+  res.redirect('/');
+});
 
-
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const express = require('express');
 const router = express.Router();
-
+const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
-})
+  console.log('Data from admin.js:', adminData.products);
+  res.sendFile(path.join(path.dirname(require.main.filename), 
+  'views', 'shop.html'));
+});
 
 module.exports = router;
