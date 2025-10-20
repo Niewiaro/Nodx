e83ab95eb4fe512ad3ab2f25ef1c146d4e252f3e
Jakub <56446320+Niewiaro@users.noreply.github.com>
2025-10-20 13:48:16 +0200

    [feat] ejs

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(express.urlencoded({extended: false}));
