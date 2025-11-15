7a8340284fc3f984ce24a977b8d5e4198b8ef5d1
Jakub <56446320+Niewiaro@users.noreply.github.com>
2025-11-15 22:07:58 +0100

    Refactor product model to use file-based storage
    Replaced in-memory product storage with asynchronous file-based storage using JSON. Updated controller methods to use async/await for saving and fetching products. This change ensures product data persists across server restarts.

--- controllers/products.js
+++ controllers/products.js
@@ -10,14 +10,14 @@ exports.getAddProduct = (req, res, next) => {
     });
 };
 
-exports.postAddProduct = (req, res, next) => {
+exports.postAddProduct = async (req, res, next) => {
     const product = new Product(req.body.title);
-    product.save();
+    await product.save();
     res.redirect("/");
 };
 
-exports.getProducts = (req, res, next) => {
-    const products = Product.fetchAll();
+exports.getProducts = async (req, res, next) => {
+    const products = await Product.fetchAll();
     res.render('shop', {
         prods: products,
         pageTitle: 'Shop',
--- models/product.js
+++ models/product.js
@@ -1,15 +1,45 @@
-const products = [];
+const fs = require('fs').promises;
+const path = require('path');
+
+const fullName = path.join(
+    path.dirname(process.main.filename),
+    'data',
+    'products.json'
+);
+
+const getProductsFromFile = async () => {
+    try {
+        const contents = await fs.readFile(fullName, 'utf-8');
+        if (contents.length === 0) {
+            return [];
+        }
+        return JSON.parse(contents);
+    } catch (err) {
+        return [];
+    }
+};
 
 module.exports = class Product {
     constructor(title) {
         this.title = title;
-    }
-    save() {
         this.price = Math.floor(Math.random() * 90) + 10;
         this.desc = "A very nice " + this.title;
-        products.push(this);
     }
-    static fetchAll() {
+    async save() {
+        products.length = 0;
+        try {
+            const prods = await getProductsFromFile();
+            products.push(...prods);
+            products.push(this);
+            await fs.writeFile(fullName, JSON.stringify(products));
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    static async fetchAll() {
+        products.length = 0;
+        const prods = await getProductsFromFile();
+        products.push(...prods);
         return products;
     }
 };
