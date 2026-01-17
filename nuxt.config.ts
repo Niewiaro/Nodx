ca1879542426f786a82a02d626efbbe2d41e8933
Jakub <56446320+Niewiaro@users.noreply.github.com>
2026-01-17 23:13:16 +0100

    Add .env.example and update config for env vars

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   runtimeConfig: {
-    mongodbUri: "mongodb://127.0.0.1:27017/g_back_05",
-    sessionSecret: "our-very-important-session-secret-of-backend-programming",
-    csrfSecret: "1234567ThisIsSecret9876543210PAI",
+    mongodbUri: "",
+    sessionSecret: "",
+    csrfSecret: "",
     public: {    }
   },
   devtools: { enabled: true },
